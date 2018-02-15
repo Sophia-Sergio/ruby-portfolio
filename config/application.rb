eaf57ca9f506ce5c663d478a1ace3ca7cfe2c069
Sophia-Sergio <san.storres@gmail.com>
2018-02-15 17:39:49 -0300

    first heroku upload

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,9 @@ Bundler.require(*Rails.groups)
 module PortfolioST
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.generators do |g|
+      g.stylesheets false
+    end
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
